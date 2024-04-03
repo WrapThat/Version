85010a6756a07dd11b3982cbc6f52427327d66c6
Roar Andersen <Roar.Andersen@fhi.no>
2024-04-03 14:00:50 +0200

    Add support for Minimal API projects without controllers

---
+++ WrapThat.Version.Tests/WebApplicationExtensionsTest.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WrapThat.Version.Tests;
+
+[TestFixture]
+public class WebApplicationExtensionsTest : IDisposable
+{
+    private readonly WebApplication _app;
+    private readonly HttpClient _httpClient;
+    private readonly string _baseUrl;
+
+
+    public WebApplicationExtensionsTest()
+    {
+        var builder = WebApplication.CreateBuilder();
+        builder.WebHost.ConfigureKestrel(serverOptions =>
+        {
+            serverOptions.ListenAnyIP(0);
+        });
+
+        _app = builder.Build();
+        _app.MapVersionApiEndpoints();
+        _app.StartAsync().GetAwaiter().GetResult();
+
+        var port = new Uri(_app.Urls.First(url => url.StartsWith("http://"))).Port;
+        _baseUrl = $"http://localhost:{port}";
+
+        _httpClient = new HttpClient();
+    }
+
+    [TestCase("/api/info")]
+    [TestCase("/api/info/version")]
+    [TestCase("/api/info/productversion")]
+    [TestCase("/api/info/shields/version")]
+    [TestCase("/api/info/shields/productversion")]
+    [TestCase("/api/info/status")]
+    public async Task Endpoint_ReturnsNonEmptySuccessResponse(string endpoint)
+    {
+        var response = await _httpClient.GetAsync($"{_baseUrl}{endpoint}");
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        Assert.That(responseString, Is.Not.Null & Is.Not.Empty, $"Endpoint {endpoint} returned an empty or null response.");
+    }
+
+    [Test]
+    public async Task Endpoints_ShouldHaveAllowAnonymous()
+    {
+        var builder = WebApplication.CreateBuilder();
+        builder.WebHost.ConfigureKestrel(serverOptions =>
+        {
+            serverOptions.ListenAnyIP(0); // 0 betyr at en ledig port vil bli valgt automatisk
+        });
+        var app = builder.Build();
+        app.MapVersionApiEndpoints();
+        await app.StartAsync();
+
+        var dataSource = app.Services.GetRequiredService<EndpointDataSource>();
+        var endpoints = dataSource.Endpoints;
+        var allHaveAllowAnonymous = endpoints.All(endpoint => endpoint.Metadata.GetMetadata<IAllowAnonymous>() != null);
+
+        Assert.That(allHaveAllowAnonymous, Is.True, "Ikke alle endepunkter er merket med AllowAnonymous");
+    }
+
+    public void Dispose()
+    {
+        _app.StopAsync().GetAwaiter().GetResult();
+        _httpClient.Dispose();
+    }
+}
---
+++ WrapThat.Version/WebApplicationExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WrapThat.Version
+{
+    public static class WebApplicationExtensions
+    {
+        public static WebApplication MapVersionApiEndpoints(this WebApplication app)
+        {
+            app.MapGet("/api/info", context => HandleRequest(context, c => c.Info()))
+                .AllowAnonymous();
+            app.MapGet("/api/info/version", context => HandleRequest(context, c => c.Version()))
+                .AllowAnonymous();
+            app.MapGet("/api/info/productversion", context => HandleRequest(context, c => c.ProductVersion()))
+                .AllowAnonymous();
+            app.MapGet("/api/info/shields/version", context => HandleRequest(context, c => c.InfoShields()))
+                .AllowAnonymous();
+            app.MapGet("/api/info/shields/productversion", context => HandleRequest(context, c => c.ProductVersionShields()))
+                .AllowAnonymous();
+            app.MapGet("/api/info/status", context => HandleRequest(context, c => c.Status()))
+                .AllowAnonymous();
+            return app;
+        }
+
+        private static async Task HandleRequest<T>(HttpContext context, Func<InfoController, ActionResult<T>> action)
+        {
+            var controller = new InfoController();
+            var actionResult = action(controller);
+            await WriteResultAsync(context, actionResult);
+        }
+
+        private static async Task WriteResultAsync<T>(HttpContext context, ActionResult<T> actionResult)
+        {
+            if (actionResult.Result is ObjectResult result)
+            {
+                context.Response.StatusCode = (int)result.StatusCode!;
+                await context.Response.WriteAsJsonAsync(result.Value);
+            }
+            else
+            {
+                if (actionResult.Result is StatusCodeResult statusCodeResult)
+                {
+                    context.Response.StatusCode = statusCodeResult.StatusCode;
+                }
+                else if (actionResult.Value != null)
+                {
+                    await context.Response.WriteAsJsonAsync(actionResult.Value);
+                }
+                else
+                {
+                    context.Response.StatusCode = StatusCodes.Status204NoContent;
+                }
+            }
+        }
+    }
+}
